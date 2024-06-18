2213983e450b3a033e8076265ed9007b56247f4b
Devjadsonf2 <jadsonnn@hotmail.com>
2024-06-18 09:47:01 -0300

    Resolvi os exercícios do capítulo 10

---
+++ cap10/js/resp10_b.js
@@ -0,0 +1,32 @@
+const frm = document.querySelector("form")
+const dvNome = document.querySelector("#divNome")
+
+frm.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const nome = frm.inNome.value.trim() // trim(): remove espaços em branco (no início e no final)
+
+    const partes = nome.split(" ")
+
+    if (partes.length == 1) {
+        alert("Informe nome completo.")
+        frm.inNome.focus()
+        return
+    }
+
+    // para retirar os h3 já existentes (se clicou uma segunda vez no botão)
+    const listaH3 = document.querySelectorAll("h3")
+    for (let i = listaH3.length - 1; i >= 0; i--) {
+        dvNome.removeChild(listaH3[i])
+    }
+
+    const cores = ["blue","yellow","red","green","orange","chocolate","deeppink","purpple","violet","aquamarine"]
+    for (const parte of partes) {
+        const h3 = document.createElement("h3")
+        const texto = document.createTextNode(parte)
+        h3.appendChild(texto)
+        const corSorteada = Math.floor(Math.random()*10)
+        h3.style.color = cores[corSorteada]
+        dvNome.appendChild(h3)
+    }
+})
