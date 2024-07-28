2c4cf7dd49750e70b6892b311b23629a5b04a043
Devjadsonf2 <jadsonnn@hotmail.com>
2024-07-28 12:49:07 -0300

    Update

--- cap11/js/ex11_3.js
+++ cap11/js/ex11_3.js
@@ -3,5 +3,32 @@ const frm = document.querySelector("form");
 frm.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    
+    // obtém conteúdo dos campos (.trim() remove espaços na palavra no início e fim)
+    const palavra = frm.inPalavra.value.trim();
+    const dica = frm.inDica.value;
+
+    // valida preenchimento (palavra não deve possuir espaço em branco no meio)
+    if (palavra.includes(" ")) {
+        alert("Informe uma palavra válida (sem espaços)");
+        frm.inPalavra.focus();
+        return;
+    }
+
+    // se já existem dados em localStorage,grava conteúdo anterior + ";" + palavra / dica
+    if (localStorage.getItem("jogoPalavra")) {
+        localStorage.setItem("jogoPalavra", localStorage.getItem("jogoPalavra") + ";" + palavra);
+        localStorage.setItem("jogoDica", localStorage.getItem("jogoDica") + ";" + dica);
+    } else {
+        // se não, é a primeira inclusão: grava apenas a palavra / dica
+        localStorage.setItem("jogoPalavra", palavra);
+        localStorage.setItem("jogoDica", dica);
+    }
+
+    // verifica se salvou
+    if (localStorage.getItem("jogoPalavra")) {
+        alert(`Ok! Palavra ${palavra} cadastrada com sucesso. `);
+    }
+
+    frm.reset(); // limpa o form
+    frm.inPalavra.focus(); // joga foco em inPalavra
 });
